b8ecf03de9e5563e34c8a94569690e69ec1bf474
joe@internpursuit.tech <joe@internpursuit.tech>
2022-08-12 16:24:40 -0400

    added animations to movement

--- main.js
+++ main.js
@@ -15,11 +15,12 @@ window.addEventListener("load", () => {
         constructor(width, height) {
             this.width = width;
             this.height = height;
+            this.groundMargin = 50;
             this.player = new Player(this);
             this.input = new InputHandler();
         }
-        update() {
-            this.player.update(this.input.keys);
+        update(deltaTime) {
+            this.player.update(this.input.keys, deltaTime);
         }
         draw(context) {
             this.player.draw(context)
@@ -27,6 +28,7 @@ window.addEventListener("load", () => {
     }
 
     const game = new Game(canvas.width, canvas.height);
+    let lastTime = 0;
     
     // const playerImage = new Image();
     // playerImage.src = './player/shadow_dog.png';
@@ -92,11 +94,13 @@ window.addEventListener("load", () => {
     //     spriteAnimations[state.name] = frames;
     // });
     
-    const animate = () => {
+    const animate = (timeStamp) => {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.update();
+        game.update(deltaTime);
         game.draw(ctx);
         requestAnimationFrame(animate);
     }
-    animate();
+    animate(0);
 })
--- playerStates.js
+++ playerStates.js
@@ -2,6 +2,7 @@ const states = {
     SITTING: 0,
     RUNNING: 1,
     JUMPING: 2,
+    FALLING: 3
 }
 
 class State {
@@ -16,6 +17,9 @@ export class Sitting extends State {
         this.player = player;
     }
     enter() {
+        // stop blinking
+        this.player.frameX = 0;
+        this.player.maxFrame = 4;
         this.player.frameY = 5;
     }
     handleInput(input) {
@@ -32,12 +36,55 @@ export class Running extends State {
     }
 
     enter() {
+        this.player.frameX = 0;
+        this.player.maxFrame = 8;
         this.player.frameY = 3;
     }
 
     handleInput(input) {
         if (input.includes('ArrowDown')) {
             this.player.setState(states.SITTING);
+        } else if (input.includes('ArrowUp')) {
+            this.player.setState(states.JUMPING)
+        }
+    }
+}
+
+export class Jumping extends State {
+    constructor(player) {
+        super('JUMPING');
+        this.player = player;
+    }
+
+    enter() {
+        if (this.player.onGround()) this.player.vy -= 27;
+        this.player.frameX = 0;
+        this.player.maxFrame = 6;
+        this.player.frameY = 1;
+    }
+
+    handleInput(input) {
+        if (this.player.vy > this.player.weight) {
+            this.player.setState(states.FALLING);
+        }
+    }
+}
+
+export class Falling extends State {
+    constructor(player) {
+        super('FALLING');
+        this.player = player;
+    }
+    
+    enter() {
+        this.player.frameX = 0;
+        this.player.maxFrame = 6;
+        this.player.frameY = 2;
+    }
+
+    handleInput(input) {
+        if (this.player.onGround()) {
+            this.player.setState(states.RUNNING);
         }
     }
 }
